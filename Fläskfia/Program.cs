cc0b1a18828bdaf6e4d8bc02925a10c2f10ed1a2
JoakimFaktare <joakim.schroeder@live.se>
2022-10-14 11:49:01 +0200

    Add project files.

---
+++ Fläskfia/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Fläskfia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jahaja");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
